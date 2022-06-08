da73515718eccaa3342a612b81d62e43d769d360
Carlos <carlosaltirado@gmail.com>
2022-06-07 21:42:10 -0500

    almostdone

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 console.log('javascript connection')
 
 
-let songSelection = document.querySelector('#songplaying');
-
-let input = document.querySelector("#artistSearch"); //targetting input box
+let audio = document.querySelector('#songplaying');
 let button = document.querySelector("#searchButton"); // targetting searchbutton
+let input = document.querySelector("#artistSearch"); //targetting input box
 let results = document.getElementById("showresults");
 console.log(results)
-//let album = document.querySelector(".album-section");
+
 
 
 button.addEventListener("click", function(event){ //we're giving click event listener to button search
@@ -32,23 +31,51 @@ button.addEventListener("click", function(event){ //we're giving click event lis
         console.error(err);
         });
         
-}) 
+})
 
 
 
 function previewSong(searchData){
     for (let song of searchData){
-    let songName = song.trackname;
+    let songName = song.trackName;
     let artistName = song.artistName;
     let source = song.previewUrl;
 
-    let previewSongSource = document.createElement('source');
-    previewSongSource.src = source;
-   // songPreview.appendChild(previewSongSource);
+
+
+    let songcontainer = document.createElement("div") //opening container
+        //how to give style to the container?
+
+    let imageAlbum = document.createElement("img")
+    //how to give style to the image?
+    imageAlbum.src = song.artworkUrl100
+    songcontainer.appendChild(imageAlbum)
+
+
 
     let newSong = document.createElement('div');
+    //how to give style to the text?
     newSong.innerText = `${songName} By ${artistName}`;
-    results.appendChild(newSong)
+    songcontainer.appendChild(newSong)
 
+
+    // adding event click listener to the image, getting audio source to play audio after click
+    imageAlbum.addEventListener("click", function(event){ 
+        console.log(source)
+
+
+// ------ things to achieve CLEARING results? ---------------
+
+
+    //we created-built a source element and append the audiotag so we can listen the songs
+    let sourceElement = document.createElement('source')
+    sourceElement.src = source
+    audio.appendChild(sourceElement)
+
+    audio.autoplay = true
+
+    })
+    results.appendChild(songcontainer) //closing container
 }}
+
 
